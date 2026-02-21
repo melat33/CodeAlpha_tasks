42f824c4e8fb648c7b721bc6b00e6763acd41a83
Melat Tewachew <melattewachew@gmail.com>
2026-02-21 16:31:18 +0300

    model builder

--- Emotional_Recognition_from_speech/src/model_builder.py
+++ Emotional_Recognition_from_speech/src/model_builder.py
@@ -7,6 +7,7 @@ class EmotionRecognitionModel:
     
     def build(self, input_shape, num_classes=8):
         model = models.Sequential([
+             # ✅ CNN LAYERS 
             layers.Reshape((input_shape[0]//4, 4, 1), input_shape=input_shape),
             
             layers.Conv2D(32, (3, 3), padding='same', activation='relu'),
@@ -19,6 +20,7 @@ class EmotionRecognitionModel:
             layers.MaxPooling2D((2, 2)),
             layers.Dropout(0.25),
             
+            # ✅ PREPARE FOR LSTM
             layers.Reshape((-1, 64)),
             layers.LSTM(64, return_sequences=True, dropout=0.2),
             layers.LSTM(32, dropout=0.2),
